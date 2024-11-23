acc8a5ed51eedf1afa3daa92dedb71e9a8cdabaf
Sunny Mishra <mishra.sunny@gmail.com>
2024-11-23 13:55:17 -0500

    static code analysis

--- page-tracker/src/page_tracker/app.py
+++ page-tracker/src/page_tracker/app.py
@@ -1,10 +1,11 @@
+import os
+from functools import cache
+
 from flask import Flask
 from redis import Redis, RedisError
-from functools import cache
-import os
-
 
 app = Flask(__name__)
+
 
 @app.get("/")
 def index():
@@ -15,6 +16,7 @@ def index():
         return "Sorry, something went wrong \N{pensive face}", 500
     else:
         return f"This page has been viewed {page_views} times"
+
 
 @cache
 def redis():
